e304737f61319a3357049febe568cc77a9b58b9a
unknown <jgonzalez@decreditos.com>
2023-08-11 18:07:12 -0300

    agregando datos a app comerciales

--- proyecto-dc/src/views/appComerciales.js
+++ proyecto-dc/src/views/appComerciales.js
@@ -2,14 +2,18 @@
 import React from "react";
 import "../components/comerciales.css";
 import imagen1 from "../images/loginApp.png";
-import imagen2 from "../images/loginApp.png";
+import imagen2 from "../images/agenciaApp.png";
 
 
-const Card = ({ title, image }) => {
+const Card = ({ title, image, subtitle, text }) => {
   return (
     <div className="MaxiCard">
       <div className="card-content">
         <h2>{title}</h2>
+        <p className="card-subtitle">{subtitle}</p>
+        <a className="card-text" href="https://comercial.decreditos.com/" target="_blank" rel="noopener noreferrer">
+          {text}
+        </a>
       </div>
       <div className="card-image">
         <img src={image} alt={title} />
@@ -23,8 +27,20 @@ const tutorialesComerciales = () => {
       <h1 className="titulosComercial">
         En esta pagina vamos a ver tutoriales sobre App Comerciales
       </h1>
-      <Card title="El ingreso a la aplicacion de comerciales es con el mismo usuario y contraseña que utilizamos en S1 Ejecutivo " image={imagen1} />
-      <Card title="Título de la segunda tarjeta" image={imagen2} />
+      <Card
+        title="El ingreso a la aplicacion de comerciales es con el mismo usuario y contraseña que utilizamos en S1 Ejecutivo "
+        subtitle="Link para ingresar a App comerciales:"
+        text="https://comercial.decreditos.com/"
+        image={imagen1}
+      />
+      <Card
+        title="Home de App de comerciales"
+        subtitle="En el ingreso a la App, vamos a ver las agencias asignadas desde hubspot al comercial. 
+        En la card de la agencia, podemos ver datos importantes como <estado de agencia>, <Fecha de ultima visita>,
+        <Nombre completo>, <Direccion de agencia> <Ubicación (al clickear se abre en google maps)>.Ademas, al clickear
+        sobre <mas información> accedes para ver mas datos o incluso realizar otras acciones."
+        image={imagen2}
+      />
     </div>
   );
 };
